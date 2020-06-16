a057cc7e896ca92c025b8ae0ca4e16b6bab8850d
Ephraim Gitonga <39718444+gitonga24@users.noreply.github.com>
2020-06-15 20:09:54 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-const bioButton = document.querySelector("#toggleBtn");
-const content = document.querySelector(".hide");
+//Code for the image Carousel javascript.
 
-bioButton.addEventListener("click", function() {
-    content.classList.toggle("show");
-});
-
-
-
-
-//image Carousel javascript.
 var i = 0;
 var images = [];
 var time = 3000;
 
-images[0] = './images/virtualPetShelter.jpg';
-images[1] = './images/fortuneTeller.jpg'
-images[2] = './images/GuessingGame.jpg';
-images[3] = './images/VPACode.jpg';
-images[4] = './images/GuessingGameCode.jpg';
+images[0] = './images/profilePhoto.jpg';
+images[1] = './images/profilePhoto2.jpg';
+images[2] = './images/javascript.jpg';
+images[3] = './images/JPA.jpg';
+images[4] = './images/css.jpg';
 
 function changeImg() {
-    document.getElementById('slide').src = images[i];
+    document.getElementById('slide').src = images[i];;
 
     let carousels = document.getElementsByClassName("image-carousel");
 
@@ -31,42 +22,8 @@ function changeImg() {
     }
 
     setTimeout("changeImg()", time);
+
 }
 
 //call the function changeImg().
 window.onload = changeImg;
-
-
-
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
-
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-
-            content.style.display = "block";
-
-        }
-
-    });
-
-}
